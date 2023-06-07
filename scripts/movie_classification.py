a2216f0f2b3685274885b280ea5f388317c5d9a4
Shane Baldacchino <shane@baldacchino.net>
2023-06-07 17:38:55 +1000

    Create movie_classification.py

---
+++ scripts/movie_classification.py
@@ -0,0 +1,115 @@
+import time
+import os
+import tiktoken
+import openai
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+from openai.embeddings_utils import cosine_similarity
+from tenacity import retry, wait_random_exponential, stop_after_attempt
+
+
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+simplefilter(action='ignore', category=RuntimeWarning)
+
+# Load environment variables
+load_dotenv()
+
+# Configure OpenAI API
+openai.api_type = "azure"
+openai.api_version = "2022-12-01"
+openai.api_base = os.getenv('OPENAI_API_BASE')
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Define embedding model and encoding
+EMBEDDING_MODEL = 'text-embedding-ada-002'
+COMPLETION_MODEL = 'text-davinci-003'
+encoding = tiktoken.get_encoding('cl100k_base')
+
+
+print("███    ███ ██    ██  ██████  ██████      ██   ██ ███████ ██      ██████  ███████ ██████      ██████   ██████  ████████ ")
+print("████  ████  ██  ██  ██    ██ ██   ██     ██   ██ ██      ██      ██   ██ ██      ██   ██     ██   ██ ██    ██    ██    ")
+print("██ ████ ██   ████   ██    ██ ██████      ███████ █████   ██      ██████  █████   ██████      ██████  ██    ██    ██    ")
+print("██  ██  ██    ██    ██    ██ ██   ██     ██   ██ ██      ██      ██      ██      ██   ██     ██   ██ ██    ██    ██    ")
+print("██      ██    ██     ██████  ██████      ██   ██ ███████ ███████ ██      ███████ ██   ██     ██████   ██████     ██    ")
+print("")
+print("sbaldacchino@microsoft.com")                           
+                                                                                                                                                       
+                                                                                                                  
+
+
+
+df = pd.read_csv('../../data/movies/movies2.csv')
+print(df.shape)
+
+df.head()
+
+
+# add a new column to the dataframe where you put the token count of the review
+df = df.assign(token_count=df['overview'].apply(lambda x: len(encoding.encode(x))))
+
+# print the first 5 rows of the dataframe, then also the total number of tokens
+total_tokens = df['token_count'].sum()
+
+cost_for_embeddings = total_tokens / 1000 * 0.0004
+print(f"Test would cost ${cost_for_embeddings} for embeddings")
+
+@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(10))
+def get_embedding(text) -> list[float]:
+    text = text.replace("\n", " ")
+    return openai.Embedding.create(input=text, engine=EMBEDDING_MODEL)["data"][0]["embedding"]
+
+df = df.assign(embedding=df['overview'].apply(lambda x: get_embedding(x)))
+df.head()
+
+# train k-means on df embeddings
+from sklearn.cluster import KMeans
+
+n_clusters = 5
+kmeans = KMeans(n_clusters=n_clusters, init="k-means++", random_state=42)
+kmeans.fit(df['embedding'].to_list())
+df = df.assign(cluster=kmeans.labels_)
+df.head()
+
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+tsne = TSNE(
+    n_components=2, perplexity=50, random_state=42, init="random", learning_rate=200
+)
+
+matrix = np.vstack(df.embedding.values)
+print(matrix.shape)
+vis_dims2 = tsne.fit_transform(matrix)
+
+x = [x for x, y in vis_dims2]
+y = [y for x, y in vis_dims2]
+
+for category, color in enumerate(["purple", "green", "red", "blue","yellow", 'black', 'orange', 'brown', 'pink', 'grey']):
+    xs = np.array(x)[df.cluster == category]
+    ys = np.array(y)[df.cluster == category]
+    plt.scatter(xs, ys, color=color, alpha=0.3)
+
+    avg_x = xs.mean()
+    avg_y = ys.mean()
+
+    plt.scatter(avg_x, avg_y, marker="x", color=color, s=100)
+
+## Break 
+    # take 10 movies from each cluster and write a prompt that asks what these have in common
+# ideally you would use more movies than 10, but this is just a demo
+for i in range(n_clusters-2):
+    reviews = df[df['cluster'] == i]['overview'].sample(3)
+    reviews = "\n".join(reviews.values.tolist())
+    prompt = f"Here are 5 movie descriptions:\n{reviews}Write 3 words what these have in common?"
+    #print(prompt)
+    time.sleep(10)
+    response = openai.Completion.create(engine=COMPLETION_MODEL, prompt=prompt, temperature=0.7, max_tokens=100, top_p=1, frequency_penalty=0, presence_penalty=0, stop=None)['choices'][0]['text'].strip()
+    print(f"Cluster {i} topics: {response}")
+    movies = df[df['cluster'] == i]['original_title'].sample(25)
+    print(f"Movies from cluster {i}: {', '.join(movies.values.tolist())}")
+    print("================")    
+
